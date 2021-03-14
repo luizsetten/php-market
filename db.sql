9091f67039f6355e4cf8d0f4ce5ff5768bf96dfe
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-03-14 09:11:41 -0300

    feat: add database

--- db.sql
+++ db.sql
@@ -1,4 +1,18 @@
-CREATE DATABASE concessionaria;
-USE concessionaria;
+CREATE DATABASE mercado;
+USE mercado;
 
-CREATE TABLE carros(id INT AUTO_INCREMENT NOT NULL, marca VARCHAR(255) NOT NULL, modelo VARCHAR(255) NOT NULL, ano INT(4) NOT NULL, preco FLOAT NOT NULL, cor VARCHAR(255) NOT NULL, descricao VARCHAR(255) NOT NULL, PRIMARY KEY (id), foto longblob);
+CREATE TABLE Fornecedor(CNPJ VARCHAR(14), NomeFantasia VARCHAR(30), Contato VARCHAR(14), PRIMARY KEY(CNPJ));
+
+CREATE TABLE Produto(ID INT, Nome VARCHAR(50), Quantidade INT, PreçoVenda FLOAT, PreçoCompra FLOAT, PRIMARY KEY(ID));
+
+CREATE TABLE Compra(NrNotaFiscal INT, PreçoTotal FLOAT, DataHora VARCHAR(20), PRIMARY KEY(NrNotaFiscal));
+
+CREATE TABLE Possui(NrNotaFiscal INT, IDProduto INT, Quantidade INT, PRIMARY KEY(NrNotaFiscal, IDProduto), FOREIGN KEY(NrNotaFiscal) REFERENCES Compra(NrNotaFiscal), FOREIGN KEY(IDProduto) REFERENCES Produto(ID));
+
+CREATE TABLE Funcionário(CPF VARCHAR(14), Nome VARCHAR(30), Salário FLOAT, Cargo VARCHAR(20), CondiçõesDeSaúde VARCHAR(90), PRIMARY KEY(CPF));
+
+CREATE TABLE Usuário(ID INT, CPFFuncionário VARCHAR(14), Tipo INT, Senha VARCHAR(200), PRIMARY KEY(ID), FOREIGN KEY(CPFFuncionário) REFERENCES Funcionário(CPF));
+
+ALTER TABLE Produto ADD(CNPJFornecedor VARCHAR(14), FOREIGN KEY(CNPJFornecedor) REFERENCES Fornecedor(CNPJ));
+
+ALTER TABLE Compra ADD(IDUsuário int, FOREIGN KEY(IDUsuário) REFERENCES Usuário(ID));
--- utils/mysql_connect.php
+++ utils/mysql_connect.php
@@ -1,8 +1,8 @@
 <?php
 // definições de host, database, usuário e senha
 $host = "localhost";
-$db   = "concessionaria";
+$db   = "mercado";
 $user = "root";
 $pass = "password";
 // conecta ao banco de dados
-$con = mysqli_connect($host, $user, $pass, $db) or trigger_error(mysqli_error($con),E_USER_ERROR);
+$con = mysqli_connect($host, $user, $pass, $db) or trigger_error(mysqli_error($con), E_USER_ERROR);
