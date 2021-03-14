173fe4bf587da8795a2a7343a7a9e79a0dc195fe
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2021-03-14 10:45:50 -0300

    feat: product registry working

--- cadastro.php
+++ cadastro.php
@@ -20,56 +20,24 @@
     include "navbar.php";
     ?>
     <form id="formVeiculo" method="POST" action="./utils/store.php" class="row" enctype="multipart/form-data">
-      <div class="form-group col-md-6">
-        <label>Marca:</label>
-        <select id="marca" name="marca" class="form-control custom-select">
-          <option value="">-- Selecionar --</option>
-          <option value="Chevrolet">Chevrolet</option>
-          <option value="Ford">Ford</option>
-          <option value="Hyiundai">Hyiundai</option>
-        </select>
-        <div class="alert-danger w-100 p-2 d-none">Marca é obrigatório</div>
-      </div>
-      <div class="form-group col-md-6">
-        <label>Modelo:</label>
-        <input type="text" id="modelo" name="modelo" class="form-control" value="" placeholder="Insira o nome do modelo">
-        <div class="alert-danger w-100 p-2 d-none">Modelo inválido</div>
-      </div>
-      <div class="form-group col-md-6">
-        <label>Ano:</label>
-        <input type="number" id="ano" name="ano" class="form-control" value="" placeholder="Insira o ano do modelo">
-        <div class="alert-danger w-100 p-2 d-none">Ano inválido</div>
-      </div>
-      <div class="form-group col-md-6">
-        <label>Preço:</label>
-        <input type="text" id="preco" name="preco" class="form-control" value="" placeholder="Insira o preço do modelo">
-        <div class="alert-danger w-100 p-2 d-none">Preço inválido</div>
-      </div>
-      <div class="form-group col-md-6">
-        <label>Foto:</label>
-        <!-- <input type="text" id="foto" name="foto" class="form-control" value="" placeholder="Insira o nome da foto"> -->
-        <input type="file" id="foto" name="foto" class="form-control" accept="image/x-png,image/jpeg" />
-        <div class="alert-danger w-100 p-2 d-none">Foto é obrigatório</div>
-      </div>
-      <div class="form-group col-md-6">
-        <label>Cor:</label>
-        <select id="cor" name="cor" class="form-control custom-select">
-          <option value="">-- Selecionar --</option>
-          <option value="Preto">Preto</option>
-          <option value="Branco">Branco</option>
-          <option value="Prata">Prata</option>
-          <option value="Vermelho">Vermelho</option>
-        </select>
-        <div class="alert-danger w-100 p-2 d-none">Cor é obrigatório</div>
-      </div>
       <div class="form-group col-md-12">
-        <label>Descrição:</label>
-        <textarea class="form-control" id="descricao" name="descricao" rows="10" placeholder="Insira a descrição do veículo"></textarea>
-        <div class="alert-danger w-100 p-2 d-none">Descrição é obrigatório</div>
+        <label>Nome:</label>
+        <input type="text" name="nome" id="nome" class="form-control" placeholder="Insira o nome do produto">
+        <div class="alert-danger w-100 p-2 d-none">Nome inválido</div>
+      </div>
+      <div class="form-group col-md-6">
+        <label>Preço de Compra:</label>
+        <input type="text" step="0.01" name="precoCompra" id="precoCompra" class="form-control" placeholder="Insira o preço do modelo">
+        <div class="alert-danger w-100 p-2 d-none">Preço de Compra inválido</div>
+      </div>
+      <div class="form-group col-md-6">
+        <label>Preço de Venda:</label>
+        <input type="text" step="0.01" name="precoVenda" id="precoVenda" class="form-control" placeholder="Insira o preço do modelo">
+        <div class="alert-danger w-100 p-2 d-none">Preço de Venda inválido</div>
       </div>
       <div class="form-group col-md-12 text-right">
         <button type="submit" class="btn btn-primary">
-          Cadastrar Veículo
+          Cadastrar Produto
         </button>
         <button type="reset" class="btn btn-secondary">
           Limpar
--- editar.php
+++ editar.php
@@ -34,29 +34,28 @@
     <form id="formVeiculo" method="POST" action="utils/update.php" class="row" enctype="multipart/form-objeto">
       <input type="hidden" name="id" id="id" value=<?= $objeto->ID ?>>
       <div class="form-group col-md-12">
-        <div class="form-group col-md-12">
-          <label>Nome:</label>
-          <input type="text" name="nome" id="nome" class="form-control" value="<?= $objeto->Nome ?>" placeholder="Insira o nome do produto">
-          <div class="alert-danger w-100 p-2 d-none">Nome inválido</div>
-        </div>
-        <div class="form-group col-md-6">
-          <label>Preço de Compra:</label>
-          <input type="text" step="0.01" name="preco-compra" id="preco-compra" class="form-control" value="<?= floatval($objeto->PreçoCompra) ?>" placeholder="Insira o preço do modelo">
-          <div class="alert-danger w-100 p-2 d-none">Preço inválido</div>
-        </div>
-        <div class="form-group col-md-6">
-          <label>Preço de Venda:</label>
-          <input type="text" step="0.01" name="preco-venda" id="preco-venda" class="form-control" value="<?= floatval($objeto->PreçoVenda) ?>" placeholder="Insira o preço do modelo">
-          <div class="alert-danger w-100 p-2 d-none">Preço inválido</div>
-        </div>
-        <div class="form-group col-md-12 text-right">
-          <button type="submit" class="btn btn-primary">
-            Salvar Produto
-          </button>
-          <button type="reset" class="btn btn-secondary">
-            Limpar
-          </button>
-        </div>
+        <label>Nome:</label>
+        <input type="text" name="nome" id="nome" class="form-control" value="<?= $objeto->Nome ?>" placeholder="Insira o nome do produto">
+        <div class="alert-danger w-100 p-2 d-none">Nome inválido</div>
+      </div>
+      <div class="form-group col-md-6">
+        <label>Preço de Compra:</label>
+        <input type="text" step="0.01" name="preco-compra" id="preco-compra" class="form-control" value="<?= floatval($objeto->PreçoCompra) ?>" placeholder="Insira o preço do modelo">
+        <div class="alert-danger w-100 p-2 d-none">Preço inválido</div>
+      </div>
+      <div class="form-group col-md-6">
+        <label>Preço de Venda:</label>
+        <input type="text" step="0.01" name="preco-venda" id="preco-venda" class="form-control" value="<?= floatval($objeto->PreçoVenda) ?>" placeholder="Insira o preço do modelo">
+        <div class="alert-danger w-100 p-2 d-none">Preço inválido</div>
+      </div>
+      <div class="form-group col-md-12 text-right">
+        <button type="submit" class="btn btn-primary">
+          Salvar Produto
+        </button>
+        <button type="reset" class="btn btn-secondary">
+          Limpar
+        </button>
+      </div>
     </form>
     <hr>
     <?php include "footer.php" ?>
